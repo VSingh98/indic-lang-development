a1e13e9ac8cd273715f7d31ece9ad35e9199780f
Sam Parker <71samparker@gmail.com>
2017-05-29 14:46:01 -0700

    cleaned up variables. This classifies based off of bigrams

--- classifier_stage2.py
+++ classifier_stage2.py
@@ -5,25 +5,25 @@ import random
 from ngram_splitter import ngram
 #opening the hindi text and putting its words in a list
 f = open("C:/Users/Sam/Documents/School/Lign 165/projectnext/project/hindi/90_utf.txt.norm",encoding="utf8" )
-the_text = f.read()
-words = the_text.split()
-bigrams = []
-for word in words:
-    bigrams += ngram(word,2)
-hindi_words = ([(word, 'hindi') for word in bigrams])
+hindi_corpus = f.read()
+tokenized_hindi_corpus = hindi_corpus.split()
+hindi_bigrams = []
+for word in tokenized_hindi_corpus:
+    hindi_bigrams += ngram(word,2)
+tupled_hindi_words = ([(word, 'hindi') for word in hindi_bigrams])
 f.close()
 #opening the marathi text and putting its words in a list
 g = open("C:/Users/Sam/Documents/School/Lign 165/projectnext/project/marathi/astro1.txt.norm",encoding="utf8")
-marathi_text = g.read()
-other_words = marathi_text.split()
-other_bigrams = []
-for word in other_words:
-    other_bigrams += ngram(word,2)
-marathi_words= ([(word, 'marathi') for word in other_bigrams])
+marathi_corpus = g.read()
+tokenized_marathi_corpus = marathi_corpus.split()
+marathi_bigrams = []
+for word in tokenized_marathi_corpus:
+    marathi_bigrams += ngram(word,2)
+tupled_marathi_words= ([(word, 'marathi') for word in marathi_bigrams])
 g.close()
 
 #combining to one big set for testing and development.
-the_set = marathi_words + hindi_words
+the_set = tupled_marathi_words + tupled_hindi_words
 random.shuffle(the_set)
 #print (the_set)
 #print (len(the_set))
