2ac821b9b32f48e71602fe677b0087eba9a2990c
Vansh <vanshchaudhary@gmail.com>
2017-05-29 17:43:08 -0700

    hopefully this works

--- confusability_matrix.py
+++ confusability_matrix.py
@@ -4,27 +4,27 @@ def print_matrix():
 	print "\tsanskrit\tpali"
 
 	print " words"
-	print "Hindi\t" + str(create_NaiveBayesClassifier('hs',1000000)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('hp',1000000)[1])[:5]
-	print "Marathi\t" + str(create_NaiveBayesClassifier('ms',1000000)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('mp',1000000)[1])[:5]
+	print "Hindi\t" + str(1-create_NaiveBayesClassifier('hs',1000000)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('hp',1000000)[1])[:5]
+	print "Marathi\t" + str(1-create_NaiveBayesClassifier('ms',1000000)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('mp',1000000)[1])[:5]
 
 	print "\n 1 character grams"
-	print "Hindi\t" + str(create_NaiveBayesClassifier('hs',1000000, False, 1)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('hp',1000000, False, 1)[1])[:5]
-	print "Marathi\t" + str(create_NaiveBayesClassifier('ms',1000000, False, 1)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('mp',1000000, False, 1)[1])[:5]
+	print "Hindi\t" + str(1-create_NaiveBayesClassifier('hs',1000000, False, 1)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('hp',1000000, False, 1)[1])[:5]
+	print "Marathi\t" + str(1-create_NaiveBayesClassifier('ms',1000000, False, 1)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('mp',1000000, False, 1)[1])[:5]
 
 	print "\n 2 character grams"
-	print "Hindi\t" + str(create_NaiveBayesClassifier('hs',1000000, False, 2)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('hp',1000000, False, 2)[1])[:5]
-	print "Marathi\t" + str(create_NaiveBayesClassifier('ms',1000000, False, 2)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('mp',1000000, False, 2)[1])[:5]
+	print "Hindi\t" + str(1-create_NaiveBayesClassifier('hs',1000000, False, 2)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('hp',1000000, False, 2)[1])[:5]
+	print "Marathi\t" + str(1-create_NaiveBayesClassifier('ms',1000000, False, 2)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('mp',1000000, False, 2)[1])[:5]
 
 	print "\n 3 character grams"
-	print "Hindi\t" + str(create_NaiveBayesClassifier('hs',1000000, False, 3)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('hp',1000000, False, 3)[1])[:5]
-	print "Marathi\t" + str(create_NaiveBayesClassifier('ms',1000000, False, 3)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('mp',1000000, False, 3)[1])[:5]
+	print "Hindi\t" + str(1-create_NaiveBayesClassifier('hs',1000000, False, 3)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('hp',1000000, False, 3)[1])[:5]
+	print "Marathi\t" + str(1-create_NaiveBayesClassifier('ms',1000000, False, 3)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('mp',1000000, False, 3)[1])[:5]
 
 	print "\n 4 character grams"
-	print "Hindi\t" + str(create_NaiveBayesClassifier('hs',1000000, False, 4)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('hp',1000000, False, 4)[1])[:5]
-	print "Marathi\t" + str(create_NaiveBayesClassifier('ms',1000000, False, 4)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('mp',1000000, False, 4)[1])[:5]
+	print "Hindi\t" + str(1-create_NaiveBayesClassifier('hs',1000000, False, 4)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('hp',1000000, False, 4)[1])[:5]
+	print "Marathi\t" + str(1-create_NaiveBayesClassifier('ms',1000000, False, 4)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('mp',1000000, False, 4)[1])[:5]
 
 	print "\n 5 character grams"
-	print "Hindi\t" + str(create_NaiveBayesClassifier('hs',1000000, False, 5)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('hp',1000000, False, 5)[1])[:5]
-	print "Marathi\t" + str(create_NaiveBayesClassifier('ms',1000000, False, 5)[1])[:5] + '\t\t' + str(create_NaiveBayesClassifier('mp',1000000, False, 5)[1])[:5]
+	print "Hindi\t" + str(1-create_NaiveBayesClassifier('hs',1000000, False, 5)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('hp',1000000, False, 5)[1])[:5]
+	print "Marathi\t" + str(1-create_NaiveBayesClassifier('ms',1000000, False, 5)[1])[:5] + '\t\t' + str(1-create_NaiveBayesClassifier('mp',1000000, False, 5)[1])[:5]
 
 print_matrix()
