9882c92687d645f651ab9f33e75c37e314cf56f4
Vansh <vanshchaudhary@gmail.com>
2017-06-04 20:33:47 -0700

    confusability matrix to file

--- confusability_matrix.py
+++ confusability_matrix.py
@@ -1,9 +1,10 @@
 from create_classifier import create_NaiveBayesClassifier
 from itertools import combinations
+import codecs
 
 TRAINING_SIZ = 10000000
 
-def create_classifiers(fileName):
+def create_classifiers(fileName, showFeatures=False):
 
 	line = '================================================================='
 	lang_codes = {'h':'hindi', 's': 'sanskrit', 'm': 'marathi', 'p': 'pali'}
@@ -14,32 +15,36 @@ def create_classifiers(fileName):
 
 	c_list = list()
 
-	print 'features: words'
-	for combo in combos:
-		classifier, accuracy, size = create_NaiveBayesClassifier(''.join(combo), TRAINING_SIZ)
+	with codecs.open(fileName, "w", encoding='utf8') as f:
 
-		print '{:>10}/{:10} epsilon: {:4.3f}\t siz: {:<10}'.format(\
-			lang_codes[combo[0]], lang_codes[combo[1]], 1-accuracy, size*2)
-
-		classifier.show_most_informative_features(10)
-
-	print '\n'+line+'\n'
-
-	for n in range(1,7):
-		
-		print 'features: {}-char grams'.format(n)
-
+		f.write('features: words\n')
 		for combo in combos:
-			classifier, accuracy, size = create_NaiveBayesClassifier(''.join(combo), \
-				TRAINING_SIZ, False, n)
+			classifier, accuracy, size = create_NaiveBayesClassifier(''.join(combo), TRAINING_SIZ)
 
-			print '{:>10}/{:10} epsilon: {:4.3f}\t siz: {:<8}'.format(\
-				lang_codes[combo[0]], lang_codes[combo[1]], 1-accuracy, size*2)
+			f.write('{:>10}/{:10} epsilon: {:4.3f}\t siz: {:<10}\n'.format(\
+				lang_codes[combo[0]], lang_codes[combo[1]], 1-accuracy, size*2))
 
-			classifier.show_most_informative_features()
+			if showFeatures:
+				informative_features = str(classifier.show_most_informative_features())
+				f.write(informative_features + '\n')
 
-		print '\n'+line+'\n'
-	return
+			f.write('\n'+line+'\n')
 
+		for n in range(1,7):
+			
+			f.write('features: {}-char grams\n'.format(n))
+
+			for combo in combos:
+				classifier, accuracy, size = create_NaiveBayesClassifier(''.join(combo), \
+					TRAINING_SIZ, False, n)
+
+				f.write('{:>10}/{:10} epsilon: {:4.3f}\t siz: {:<8}\n'.format(\
+					lang_codes[combo[0]], lang_codes[combo[1]], 1-accuracy, size*2))
+
+				if showFeatures:
+					informative_features = str(classifier.show_most_informative_features())
+					f.write(informative_features + '\n')
+
+				f.write('\n'+line+'\n')
 
 create_classifiers()
